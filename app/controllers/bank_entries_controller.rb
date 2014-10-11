416b42805c00676079fc56808d2ef5464ee5c9df
Marc Pursals <marc@marcpursals.com>
2014-10-11 17:47:15 +0200

    added filters to bank_entries

--- app/controllers/bank_entries_controller.rb
+++ app/controllers/bank_entries_controller.rb
@@ -1,12 +1,31 @@
 class BankEntriesController < ApplicationController
   def index
-    actual_bank_entry = BankEntry.find(params[:id]) if params[:id].present?
-    @bank_entries = BankEntry.all.order("bank_date DESC")
-    @proposed_invoices = proposed_invoices(params) if params && params[:matched_id].nil?
-    @current_matched_invoice = Invoice.find(params[:matched_id]) if params[:matched_id]
+
+    get_bank_entries(params)
+
+    if params[:id] && actual_bank_entry = BankEntry.find(params[:id])
+      matched_id = params[:matched_id] ? params[:matched_id] : actual_bank_entry.invoice_id
+      @current_matched_invoice = Invoice.find(matched_id) if matched_id
+      match_invoice(params, actual_bank_entry) if matched_id &&
+        actual_bank_entry.unmatched?
+    end
+
+    @proposed_invoices = proposed_invoices(params) if params &&
+      params[:matched_id].nil? && (actual_bank_entry.nil? || actual_bank_entry.unmatched?)
   end
 
   def proposed_invoices(params)
     Invoice.where(total_amount: params[:amount]).order("issue_date DESC")
+  end
+
+  def match_invoice(params, actual_bank_entry)
+    actual_bank_entry.update(invoice_id: params[:matched_id])
+  end
+
+  def get_bank_entries(params={})
+    return @bank_entries = BankEntry.all.order("bank_date DESC") if params.blank? || !params[:filter]
+    @bank_entries = BankEntry.all.order("bank_date DESC") if params[:filter] == "all"
+    @bank_entries = BankEntry.inbounds.order("bank_date DESC") if params[:filter] == "incomes"
+    @bank_entries = BankEntry.outbounds.order("bank_date DESC") if params[:filter] == "outcomes"
   end
 end
--- app/models/bank_entry.rb
+++ app/models/bank_entry.rb
@@ -3,7 +3,17 @@ class BankEntry < ActiveRecord::Base
   belongs_to :bank_account
   belongs_to :invoice
 
-  validates_presence_of :bank_account_id
+  validates_presence_of :bank_account
 
   scope :unmatched, lambda { where("bank_account_id is NOT NULL and bank_account_id != ''") }
+  scope :inbounds, lambda { where("amount < 0") }
+  scope :outbounds, lambda { where("amount >= 0") }
+
+  def matched?
+    invoice_id.present?
+  end
+
+  def unmatched?
+    !matched?
+  end
 end
